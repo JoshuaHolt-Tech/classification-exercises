cb3bc285c9bde7388e4c8c5a676b3ff98ab3e4fd
JoshuaHolt-Tech <116033033+JoshuaHolt-Tech@users.noreply.github.com>
2022-12-16 10:03:12 -0600

    Updated prep_telco function to remove object type columns.

--- prepare.py
+++ prepare.py
@@ -13,10 +13,6 @@ def prep_telco(working_df):
     cleaner set of data.
     """
     
-    #Removes columns which provide little information or are duplicate:
-    cols_to_drop = ['customer_id', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id']    
-    working_df.drop(columns=cols_to_drop, inplace = True)
-    
     #Total_charges has null entries and they are replaced with 0.
     #The assumption is these are new accounts where payment has not been made yet.
     working_df['total_charges'] = working_df['total_charges'].replace(' ', 0).astype('float')
@@ -28,10 +24,11 @@ def prep_telco(working_df):
             working_df[col] = working_df[col].replace({'Yes':True,'No':False}).astype('int')
      
     #Adds dummy columns for columns that have multiple categories.
-    dummy_df = (pd.get_dummies(working_df[['gender', 'streaming_movies', 'streaming_tv' , 'tech_support', 'multiple_lines',
-                                           'online_backup', 'online_security', 'device_protection', 'payment_type',
-                                           'internet_service_type', 'contract_type']], drop_first=True))
+    dummy_df = (pd.get_dummies(working_df[['gender', 'streaming_movies', 'streaming_tv' , 'tech_support', 'multiple_lines', 'online_backup', 'online_security', 'device_protection', 'payment_type', 'internet_service_type', 'contract_type']], drop_first=True))
     
+    #Removes columns which provide little information or are duplicate:
+    cols_to_drop = ['gender', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'contract_type', 'payment_type', 'payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id', 'internet_service_type']    
+    working_df.drop(columns=cols_to_drop, inplace = True)
     #Adds the two DataFrames back together for output:
     working_df = pd.concat([working_df, dummy_df], axis=1)
     
